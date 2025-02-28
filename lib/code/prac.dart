1291db904f4c98be6c26905593071e52a7949fd2
Abuessa265 <abuessastudent1999@gmail.com>
2025-02-28 21:28:45 +0600

    first commit

--- lib/code/prac.dart
+++ lib/code/prac.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Prac extends StatefulWidget {
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/code/prac.dart';
 import 'package:flutter_application_1/widgets/container.dart';
 
 void main() {
-  runApp(
-    MyApp(),
-  );
+  runApp(DevicePreview(
+    builder: (context) => MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,13 +23,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Essential code'),
+      ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'hello i am a software engineer now i job a software compny and i want to be a grate engineer in the world so i need hard work for a grate engineer if you hard work then you will be success otherwise you cannot success',
+                'Hello i am a software engineer now i job a software compny and i want to be a grate engineer in the world so i need hard work for a grate engineer if you hard work then you will be success otherwise you cannot success',
                 textAlign: TextAlign.center,
                 maxLines: 2,
                 style: TextStyle(overflow: TextOverflow.ellipsis),
